d4b792dc263f48d54f827b945aa3bdb0f8a1f5e4
nkzttt <free.nkzt@gmail.com>
2022-05-16 22:23:31 +0900

    added alias.

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
       },
     },
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
