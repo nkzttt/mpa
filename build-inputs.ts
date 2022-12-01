a2592d1149e52a685a947775d3b467ba77a22bf3
nkzttt <free.nkzt@gmail.com>
2022-12-01 22:16:50 +0900

    added articles.

--- build-inputs.ts
+++ build-inputs.ts
@@ -13,6 +13,10 @@ const inputs = {
     "books/illustrated-introduction-to-economics.html"
   ),
   bestWayToRest: path.resolve(__dirname, "books/best-way-to-rest.html"),
+  techniqueOfGoodQuestion: path.resolve(
+    __dirname,
+    "books/technique-of-good-question.html"
+  ),
 };
 
 export const titles: Record<keyof typeof inputs, string> = {
@@ -22,6 +26,7 @@ export const titles: Record<keyof typeof inputs, string> = {
   predictablyIrrational: "予想どおりに不合理",
   illustratedIntroductionToEconomics: "たった1つの図でわかる! 図解経済学入門",
   bestWayToRest: "最高の休息法",
+  techniqueOfGoodQuestion: "「良い質問」をする技術",
 };
 
 export default inputs;
---
+++ src/pages/books/TechniqueOfGoodQuestion.ts
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import TechniqueOfGoodQuestion from "./TechniqueOfGoodQuestion.vue";
+
+createApp(TechniqueOfGoodQuestion).mount("#app");
